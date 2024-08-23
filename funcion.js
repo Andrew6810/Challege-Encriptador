f540ed3b24e3e2a5d5bef5b59ef866b5b5f724a0
Andrey Cespedes <andrey42002@gmail.com>
2024-08-23 14:40:14 -0500

    arreglando alerts

--- funcion.js
+++ funcion.js
@@ -11,7 +11,7 @@ function encriptar() {
       .replace(/o/gi, "ober")
       .replace(/u/gi, "ufat");
 
-    if (texto.value != 0) {
+    if (texto.length != 0) {
       document.getElementById("texto").value = textoCifrado;//volvermos a acceder al area del texto ya que la variable texto ya fue modificada
       tituloMensaje.textContent = "Texto encriptado con éxito";
       parrafo.textContent = "";
@@ -20,6 +20,33 @@ function encriptar() {
       muñeco.src = "./img/Muñeco.png";
       tituloMensaje.textContent = "Ningun mensaje fue encontrado";
       parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-        alert("Debes ingresa un texto");
+      swal("Ooops!", "Debes ingresar un texto para encriptar", "warning");
     }
+}
+
+function desencriptar() {
+  //Definimos las variables tambien para esta funcion 
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("viñeta");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/Desencriptado.png";
+  }else{
+    muñeco.src = "./img/Muñeco.png";
+    tituloMensaje.textContent = "Ningun mensaje fue encontrado!";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto para desencriptar", "warning");
+  }
 }
